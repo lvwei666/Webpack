ba989fa78d981f127b2a82be814a4e9e5249b945
lvwei666 <643398289@qq.com>
2019-05-25 19:27:40 +0800

    .

---
+++ babel/index.js
@@ -0,0 +1,16 @@
+let a = 1,
+    b = 2;
+    let c = a + b;
+
+// [1,2,3].map(item => `${item}item`)
+
+// class A {
+//     constructor() {
+//         this.name = 'name';
+//     }
+// }
+
+let mike ={
+    name: 'mike',age: 40
+};
+mike = {...mike,sex:'男'}
